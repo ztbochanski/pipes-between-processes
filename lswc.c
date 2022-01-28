73cafe25a0a6f68bf24279914eab81c35adf3456
zach <ztbochanski@gmail.com>
2022-01-28 14:06:42 -0800

    add CL parsing and declare file descriptor array

--- lswc.c
+++ lswc.c
@@ -1,9 +1,22 @@
+// Zachary Bochanski
+// Use pipe system call for interprocess communication between ls utility and wc utility
+// This file is litered with comments for personal learning/note taking
+
 #include <stdio.h>  // for printf(), perror()
 #include <stdlib.h> // for exit()
 #include <unistd.h> // for fork(), execlp(), close()
 
-// 1. parse the command line
-// 2. make a pipe in the parent
-// 3. fork
-// 4. run word count with the child
-// 5. parent runs ls
+int main(int argc, char **argv)
+{
+  // 1. parse the command line
+  int count;
+  for (count = 0; count < argc; count++)
+    printf("argv[%d] => %s\n", count, argv[count]);
+
+  // 2. make a pipe in the parent
+  int fd[2];
+
+  // 3. fork
+  // 4. run word count with the child
+  // 5. parent runs ls
+}
--- redirout.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>  // for printf(), perror()
-#include <stdlib.h> // for exit()
-#include <unistd.h> // for fork(), execlp(), close()
-#include <fcntl.h>  // for open()
