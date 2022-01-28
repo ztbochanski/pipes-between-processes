9cd46e1d61bd4b4206520a55b997ef9e629f9516
zach <ztbochanski@gmail.com>
2022-01-27 22:43:09 -0800

    add gameplan to lswc and imports to redirout

--- lswc.c
+++ lswc.c
@@ -1,3 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>  // for printf(), perror()
+#include <stdlib.h> // for exit()
+#include <unistd.h> // for fork(), execlp(), close()
+
+// 1. parse the command line
+// 2. make a pipe in the parent
+// 3. fork
+// 4. run word count with the child
+// 5. parent runs ls
