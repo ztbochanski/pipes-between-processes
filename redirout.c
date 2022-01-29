d1c12cbf50ddde7fb0525d3c543f59d0651830db
zach <ztbochanski@gmail.com>
2022-01-28 21:44:31 -0800

    add command line argument varables 1 - 4 1: file 2: command 3: option 1 4: option 2

--- redirout.c
+++ redirout.c
@@ -3,18 +3,20 @@
 #include <unistd.h> // for fork(), execlp(), close()
 #include <fcntl.h>  // for open()
 
-char *path = NULL;
+char *new_file = NULL;
 
 int main(int argc, char **argv)
 {
-  int count;
 
-  // 1. parse the command line
-  for (count = 0; count < argc; count++)
-  {
-    if (count == 1)
-    {
-      path = argv[count];
-    }
-  }
+  // 1. command line arguments
+  char *new_file = argv[1];
+  char *command = argv[2];
+  char *option1 = argv[3];
+  char *option2 = argv[4];
+
+  // 2. open the output file
+
+  // 3. create a pipe
+
+  // 4. fork the child process to run command
 }
